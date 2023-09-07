1b568f86b8081c7b4bd1e4bc39c46fa225bca2b7
gorkavillara <gorkavillara@gmail.com>
2023-09-07 13:14:23 +0200

    � Debug con React Native CLI

--- App.tsx
+++ App.tsx
@@ -1,118 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+/* eslint-disable react-native/no-inline-styles */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Alert, Button, TouchableOpacity} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+  const [contador, setContador] = useState(0);
+  // const handlePress = () => {
+  //   let i = 30;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  //   console.log(i);
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  //   Alert.alert('Hola mundo');
+  // };
+  const sumaContador = () => {
+    // setContador(contador + 1);
+    // setContador(contador + 1);
+    // setContador(contador + 1);
+    let i = 0;
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    i++;
+    console.log(contador);
+    setContador(anterior => anterior + 1);
+    setContador(anterior => anterior + 1);
+    setContador(anterior => anterior + 1);
   };
 
+  const restaContador = () => setContador(contador - 1);
+
+  // let valor = 1;
+  // const sumaValor = () => {
+  //   valor++;
+  //   console.log(valor);
+  // };
+  // const restaValor = () => valor--;
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      {/* <View>
+        <Text style={{color: '#fdfdfd'}}>Hola mundo</Text>
+        <Button title="Press Me" onPress={handlePress} />
+      </View> */}
+      <View>
+        <Text style={{fontSize: 24, textAlign: 'center'}}>
+          Contador con React Native
+        </Text>
+      </View>
+      <View>
+        <Text style={{fontSize: 16, textAlign: 'center'}}>
+          Valor del contador: {contador}
+        </Text>
+        <View style={{flexDirection: 'row', justifyContent: 'center', gap: 8}}>
+          <TouchableOpacity
+            onPress={restaContador}
+            style={{padding: 8, backgroundColor: '#fafafafa', borderRadius: 8}}>
+            <Text>Menos</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={sumaContador}
+            style={{padding: 8, backgroundColor: '#fafafafa', borderRadius: 8}}>
+            <Text>Más</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
