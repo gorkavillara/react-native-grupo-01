cbe14fe6dcd36f222cb2672dbb47f6a479ab60dc
gorkavillara <gorkavillara@gmail.com>
2023-09-28 13:15:51 +0200

    Tema 07 - Trabajando con React Native Paper

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import AppContextProvider from "./contexts/AppContextProvider"
 import ToggleDarkMode from "./components/common/ToggleDarkMode"
 import AppLayout from "./layouts/AppLayout"
+import { PaperProvider } from "react-native-paper"
 import StyledScreen from "./screens/StyledScreen"
+import PaperScreen from "./screens/PaperScreen"
 
 function App() {
     return (
-        <AppContextProvider>
-            <AppLayout>
-                <StyledScreen />
-                <ToggleDarkMode />
-            </AppLayout>
-        </AppContextProvider>
+        <PaperProvider>
+            <AppContextProvider>
+                <AppLayout>
+                    {/* <StyledScreen /> */}
+                    <PaperScreen />
+                    <ToggleDarkMode />
+                </AppLayout>
+            </AppContextProvider>
+        </PaperProvider>
     )
 }
 
